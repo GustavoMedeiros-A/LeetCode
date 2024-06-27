5ae11408564be820409e80b9758646ba766923da
Gustavo Medeiros <70780620+GustavoMedeiros-A@users.noreply.github.com>
2024-06-27 16:42:07 -0300

    Time: 1 ms (97.37%) | Memory: 45.1 MB (82.73%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -7,20 +7,20 @@ class Solution {
 
         
         for (int i = 0; i < flowerbed.length; i++) {
-            System.out.println("index  " + i);
             if (flowerbed[i] == 0) {
                 boolean havePrev = (i == 0 || flowerbed[i - 1] == 0);
                 boolean haveNext = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);
                 if (havePrev && haveNext) {
                     flowerbed[i] = 1;
                     quantitySpace++;
-                    System.out.println(quantitySpace + "  space");
                     if (quantitySpace >= n) {
                         return true;
                     }
+                    i++;
                 }
             }
         }
-        return false;
+
+        return quantitySpace >= n;
     }
 }
