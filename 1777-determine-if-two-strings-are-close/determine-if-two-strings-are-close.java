1d56bc449b0404f5521d5a014389decb57a20dce
Gustavo Medeiros <70780620+GustavoMedeiros-A@users.noreply.github.com>
2024-07-09 09:35:53 -0300

    Time: 10 ms (78.22%) | Memory: 45.5 MB (57.19%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,42 +1,51 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        // verify length
-        if(word1.length() != word2.length()) {
+        if (word1.length() != word2.length()) {
             return false;
         }
-
-
+        
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
-
-        // Set is a collection that contains no duplicate elements.
-        Set<Character> set1 = new HashSet<Character>();
-        Set<Character> set2 = new HashSet<Character>();
-
+        int[] charCount1 = new int[26];
+        int[] charCount2 = new int[26];
+        
         for (char c : word1.toCharArray()) {
             freq1[c - 'a']++;
-            set1.add(c);
+            charCount1[c - 'a'] = 1;
         }
-
+        
         for (char c : word2.toCharArray()) {
             freq2[c - 'a']++;
-            set2.add(c);
+            charCount2[c - 'a'] = 1;
         }
-
-        if(!set1.equals(set2)) {
-            return false;
-        }
-
-
-        Arrays.sort(freq1);
-        Arrays.sort(freq2);
-
+        
+        // Check if both strings have the same unique characters
         for (int i = 0; i < 26; i++) {
-            if(freq1[i] != freq2[i]) {
+            if (charCount1[i] != charCount2[i]) {
                 return false;
             }
         }
-
+        
+        // Count the frequencies of the frequencies
+        int[] freqCount1 = new int[word1.length() + 1];
+        int[] freqCount2 = new int[word2.length() + 1];
+        
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] > 0) {
+                freqCount1[freq1[i]]++;
+            }
+            if (freq2[i] > 0) {
+                freqCount2[freq2[i]]++;
+            }
+        }
+        
+        // Compare the frequency counts
+        for (int i = 0; i <= word1.length(); i++) {
+            if (freqCount1[i] != freqCount2[i]) {
+                return false;
+            }
+        }
+        
         return true;
     }
 }
