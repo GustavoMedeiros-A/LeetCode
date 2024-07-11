c01b84b531508a6420ec993eeadd4cbe2e667e02
Gustavo Medeiros <70780620+GustavoMedeiros-A@users.noreply.github.com>
2024-07-11 08:30:10 -0300

    Time: 70 ms (60.35%) | Memory: 45.8 MB (59.20%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String removeStars(String s) {
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for(char c : s.toCharArray()) {
+            if(c == '*') {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for(char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+}
+
