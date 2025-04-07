425fafa75c45e805fc4d22f2d06c26f21feab7c2
Gustavo Medeiros <70780620+GustavoMedeiros-A@users.noreply.github.com>
2025-04-07 13:10:52 -0300

    Time: 2 ms (97.66%) | Memory: 43.2 MB (74.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,16 +3,17 @@ class Solution {
         if(s.length() != t.length()) {
             return false;
         }
-        final HashMap<Character, Integer> map = new HashMap<>();
+        int [] counts = new int[26];
+
+
         for (final char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            counts[c - 'a']++;
         }
-        
+
         for(final char c : t.toCharArray()) {
-            if(!map.containsKey(c) || map.get(c) == 0) {
+            if(--counts[c - 'a'] < 0) {
                 return false;
-            }
-            map.put(c, map.get(c) - 1);
+            };
         }
 
         return true;
